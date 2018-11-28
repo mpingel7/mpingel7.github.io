e4eb520847eb5bc1a5ae90f1e79089d084c41ab7
McKay Pingel <mpingel7@gmail.com>
2018-11-28 13:25:05 -0700

    Update

---
+++ weathersite/scripts/currentdate.js
@@ -0,0 +1,8 @@
+function currentDate(){
+var d = new Date();
+var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "'October", "November", "December"];
+var fulldate = day[d.getDay()] + ", " + d.getDate() + " " + month[d.getMonth()] + " " + d.getFullYear();
+
+document.getElementById("currentdate").innerHTML = fulldate.currentDate();
+}
