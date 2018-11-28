4040f786ec9efe867f6fb5a54e4a7a1c7ee03d5d
McKay Pingel <mpingel7@gmail.com>
2018-11-28 13:13:29 -0700

    Update

--- weathersite/js/scripts.js
+++ weathersite/js/scripts.js
@@ -17,6 +17,25 @@ weatherObject.onload = function() {
 
 } // end of function
 
+var weatherProvo = new XMLHttpRequest();
+weatherProvo.open('GET','http://api.openweathermap.org/data/2.5/weather?zip=84057,us&appid=4b16756ef2f79712167ad71f85a69a6f&units=imperial',true);
+
+weatherProvo.send();
+
+weatherProvo.onload = function() {
+    var weatherInfo = JSON.parse(weatherProvo.responseText);
+    console.log(weatherInfo);
+
+    document.getElementById('place').innerHTML = weatherInfo.name;
+    document.getElementById('currentTemp').innerHTML = weatherInfo.main.temp;
+    document.getElementById('currentWind').innerHTML = weatherInfo.wind.speed;
+
+    var iconcode = weatherInfo.weather[0].icon;
+    var icon_path = "http://openweathermap.org/img/w/" + iconcode + ".png";
+    document.getElementById('weather_icon').src = icon_path;
+
+} // end of function
+
 /* var townObject = new XMLHttpRequest();
 townObject.open('GET','//byui-cit230.github.io/weather/data/towndata.json',true);
 
