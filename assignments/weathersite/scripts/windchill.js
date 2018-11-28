f968fc7b50caae321fd7968a6082cd3a98648d46
McKay Pingel <mpingel7@gmail.com>
2018-11-28 13:44:35 -0700

    Update

--- assignments/weathersite/scripts/windchill.js
+++
@@ -1,8 +0,0 @@
-var temp= 76;
-var wind = 5;
-var speed = Math.pow(wind, 0.16);
-var chill = f = 35.74 + (0.6215 * temp) - 35.75 (speed * 0.16) + 0.4275 * temp * speed * 0.16  
-
-chill = Math.round(chill) + " °F";
-
-document.getElementById('windchill').innerHTML = chill;
